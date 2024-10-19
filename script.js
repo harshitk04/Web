138351673c0aea9d8bf11b9924b939bc272184cc
harshitk04 <harshitk04.hr@gmail.com>
2024-10-19 22:30:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('loveButton').addEventListener('click', function() {
+    // Replace the URL below with the link to your video (YouTube, Vimeo, or direct link)
+    window.location.href = "https://drive.google.com/file/d/1cjR1txy_sjntp70Bj7UJ6BytOQgB8uYT/view"; 
+});
